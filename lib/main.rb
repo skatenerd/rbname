084607b60b504cf8ceb5384a8bedac05cd2fda8d
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-11-10 11:57:44 -0600

    Extract replacement

--- lib/main.rb
+++ lib/main.rb
@@ -1,2 +1,11 @@
 class Main
+  def self.replace_all(pattern, root_path)
+    occurrences = Occurrences.find(pattern, root_path)
+
+    occurrences.each do |occurrence|
+      Replacement.replace(occurrence) do |line|
+        yield(line)
+      end
+    end
+  end
 end
--- spec/integration/main_spec.rb
+++ spec/integration/main_spec.rb
@@ -7,12 +7,9 @@ describe Main do
   it "upcases all lines containing 'upcaseme'" do
     file_path = "tmp/single_file.rb"
     File.write(file_path, "foo\nupcaseme\nbaz\n")
-    occurrences = Occurrences.find("upcaseme", "tmp")
 
-    occurrences.each do |occurrence|
-      Replacement.replace(occurrence) do |line|
-        line.upcase
-      end
+    Main.replace_all("upcaseme", "tmp") do |line|
+      line.upcase
     end
 
     `cat #{file_path}`.should == "foo\nUPCASEME\nbaz\n"
